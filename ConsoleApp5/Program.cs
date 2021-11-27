29342b58105010194985a6c91c5b7ae12b9dd56b
HP <HP@DESKTOP-7RJOEHK>
2021-11-28 00:33:40 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> alilinkedlist = new LinkedList<string>();
+
+            alilinkedlist.AddFirst("mohamed");
+            alilinkedlist.AddFirst("ali");
+            alilinkedlist.AddLast("shahat");
+            alilinkedlist.AddLast("mohamed");
+
+            Console.WriteLine("The linked List Before Removing:  ");
+
+            foreach (var item in alilinkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            alilinkedlist.Remove("mohamed");
+            alilinkedlist.RemoveLast();
+            alilinkedlist.RemoveFirst();
+
+            Console.WriteLine();
+            Console.WriteLine("The linked List After Removing: ");
+
+            foreach (var item in alilinkedlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
